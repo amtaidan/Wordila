376c141f8a1d5af5968664a67814138691caff4d
Unknown <amtaidan@comcast.net>
2017-07-17 18:01:11 -0500

    Wood background and fonts. iPhone resolution. MusicPlayer script.

---
+++ Assets/Scripts/Musicplayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Musicplayer : MonoBehaviour {
+
+	static Musicplayer instance = null;
+	// Use this for initialization
+	void Awake () {
+		if (instance != null) {
+			Destroy (gameObject);
+			print ("Duplicate music player self-destructed");
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+	}
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
